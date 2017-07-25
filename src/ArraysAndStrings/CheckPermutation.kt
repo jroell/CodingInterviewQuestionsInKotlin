182d53af1ea9201c3e97d4f9e2bed4148abd9b64
Jason Roell <jason.roell@kroger.com>
2017-07-24 21:52:14 -0400

    new files

--- src/ArraysAndStrings/CheckPermutation.kt
+++ src/ArraysAndStrings/CheckPermutation.kt
@@ -7,6 +7,27 @@ package ArraysAndStrings
 // Given two strings, write a method to decide if one is a permutation of the other.
 
 fun isPermutation(word1: String, word2: String): Boolean {
-    return false
+    if (word1.length != word2.length) return false
+    val letters = IntArray(128)
+    for (letter in word1) {
+        letters[letter.toInt()]++
+    }
+
+    for (letter in word2) {
+        letters[letter.toInt()]--
+        if (letters[letter.toInt()] < 0) {
+            return false
+        }
+    }
+
+    return true
 }
 
+fun main(args: Array<String>) {
+    val word1 = "asdf"
+    val word2 = "fdas"
+    val word3 = "zxcv"
+
+    println("Should print true: ${isPermutation(word1, word2)}")
+    println("Should print false: ${isPermutation(word1, word3)}")
+}
--- src/ArraysAndStrings/URLify.kt
+++ src/ArraysAndStrings/URLify.kt
@@ -8,7 +8,28 @@ package ArraysAndStrings
 // at the end to hold the additional characters, you are given the "true" length of the string. (Note: If
 // implementing in Java, please use a character array so that you can perform this operation in place.)
 
-fun URLify(input: String): String {
+fun URLify(input: String, realLength: Int): String {
+//    val spaceCount = 0
+//    val i = 0
+//
+//    for (letter in input){
+//        if (letter == ' '){
+//            continue
+//        }
+//    }
+//
+//    var index = realLength + spaceCount * 2
+//    if (realLength < input.length) {
+//        input[realLength] = '\0'
+//    }
+//
+//    for ()
     return input
+}
+
+fun main(args: Array<String>) {
+    val input = "Mr John Smith    "
+    val realLength = 13
+    println("Should print Mr%20John%20Smith: ${URLify(input, realLength)}")
 }
 
