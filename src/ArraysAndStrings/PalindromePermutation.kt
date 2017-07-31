2f3c9fff2e36a2e55b20ac33c488c4263d301c3c
Jason Roell <jason.roell@kroger.com>
2017-07-31 13:14:08 -0400

    Add code for Palindrome Permutation

--- src/ArraysAndStrings/PalindromePermutation.kt
+++ src/ArraysAndStrings/PalindromePermutation.kt
@@ -14,9 +14,24 @@ package ArraysAndStrings
 // Output:  True  (permutations: "taco cat", "atco cta", etc. )
 
 fun isPermutationOfAPalindrome(input: String): Boolean {
-    var isPermutationOfAPalindrome = false
+    var dictionary = mutableMapOf<Char, Int>()
+    for (letter in input){
+        if (dictionary.contains(letter)){
+            var temp = dictionary.getValue(letter)
+            dictionary.put(letter, temp + 1)
+        } else {
+            dictionary.put(letter, 1)
+        }
+    }
 
-    // implementation
+    var numberOfOdds = dictionary.filter { it.value % 2 == 0 }.count()
+    return numberOfOdds < 2
+}
 
-    return isPermutationOfAPalindrome
+fun main(args: Array<String>) {
+    var permutationOfPalindrome = "Tact Coa"
+    var notPermutationOfPalindrom = "This is not a permutation of a palindrome"
+
+    println("should print true: ${isPermutationOfAPalindrome(permutationOfPalindrome)}")
+    println("should print false: ${isPermutationOfAPalindrome(notPermutationOfPalindrom)}")
 }
