3fed77174e0106149ab186e46d60e42b4b34e2ba
Jason Roell <jason.roell@kroger.com>
2017-09-27 16:42:16 -0400

    Add solution for Compress Strings

--- src/ArraysAndStrings/StringCompression.kt
+++ src/ArraysAndStrings/StringCompression.kt
@@ -10,8 +10,56 @@ package ArraysAndStrings
 // letters (a-z).
 
 fun compressString(input: String): String {
-    var compressed = ""
+    // go through and check if prev == current
+    // if true add to count
+    // if false and count is greater than 1, append count plus char to builder
+    // once iterator reaches end check do appropriate append
+    var count = 1
+    var prev = input.first()
+    var builder = ""
+    for ((i, letter) in input.withIndex()) {
+        if (i == 0) continue
+        when (letter) {
+            prev -> {
+                count += 1
+                builder = checkEnd(i, input, builder, count, letter)
+            }
+            else -> {
+                builder = build(builder, count, prev)
+                prev = letter
+                count = 1
+                builder = checkEnd(i, input, builder, count, letter)
+            }
+        }
+    }
 
+    return builder
+}
 
-    return compressed
+private fun checkEnd(i: Int, input: String, builder: String, count: Int, letter: Char): String {
+    var builder1 = builder
+    if (i == input.lastIndex) {
+        builder1 = build(builder1, count, letter)
+    }
+    return builder1
+}
+
+private fun build(builder: String, count: Int, prev: Char): String {
+    var builder1 = builder
+    builder1 += if (count > 1) {
+        "$count$prev"
+    } else {
+        "$prev"
+    }
+    return builder1
+}
+
+fun main(args: Array<String>) {
+    val test1 = "aabcccccaa"
+    val test2 = "abaabbbcdddeffg"
+    val test3 = "abcdefgh"
+
+    println("Should print 2ab5c2a. Actual:${compressString(test1)}")
+    println("Should print ab2a3bc3de2fg. Actual:${compressString(test2)}")
+    println("Should print abcdefgh. Actual:${compressString(test3)}")
 }
