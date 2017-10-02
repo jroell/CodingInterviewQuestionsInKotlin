bce6f193b627d9baccd6be8aed4af5b7a9913a64
Jason Roell <jason.roell@kroger.com>
2017-10-02 18:46:38 -0400

    Adds solution for Number Max

--- src/ModerateDifficultQuestions/NumberMax.kt
+++ src/ModerateDifficultQuestions/NumberMax.kt
@@ -5,3 +5,9 @@ package ModerateDifficultQuestions
  */
 
 // Write a method that finds the maximum of two numbers. You should not use if-else or any other comparison operator.
+
+fun max(input1: Int, input2:Int) = Math.max(input1, input2)
+
+fun main(args: Array<String>) {
+    println("Should return 3: Actual: ${max(3,1)}")
+}
