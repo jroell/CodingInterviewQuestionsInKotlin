a867d87da5dd4de876cfb990f1682c7e65c972cc
Jason Roell <jason.roell@kroger.com>
2017-10-04 17:52:00 -0400

    Adds solution to NumberSwapper and PairsWithSums

--- src/ModerateDifficultQuestions/NumberSwapper.kt
+++ src/ModerateDifficultQuestions/NumberSwapper.kt
@@ -5,3 +5,25 @@ package ModerateDifficultQuestions
  */
 
 // Write a function to swap a number in place (that is, without temporary variables).
+
+fun swapNumbersWithBitManipulation(x: Int, y: Int): Int {
+    var x = x
+    var y = y
+    x = x xor y
+    y = x xor y
+    x = x xor y
+    return x
+}
+
+fun swapNumbers(x: Int, y: Int): Int {
+    var x = x
+    var y = y
+    x = y - x 
+    y -= x
+    x += y
+    return x
+}
+
+fun main(args: Array<String>) {
+    println("Should print 43, 73: Actual: ${swapNumbers(73, 43)}")
+}
--- src/ModerateDifficultQuestions/PairsWithSum.kt
+++ src/ModerateDifficultQuestions/PairsWithSum.kt
@@ -5,3 +5,22 @@ package ModerateDifficultQuestions
  */
 
 // Design an algorithm to find all pairs of integers within an array which sum to a specified value.
+
+fun findPairsWithSum(arr: Array<Int>, sum: Int): List<Pair<Int, Int>> {
+    // is there another number that I can add to that equals the sum
+    // we know that if we have a set made up of the difference between our number and the sum
+    // then we know all the numbers we are looking for
+    // we should then ask if the number it plus (any) from the set == that sum
+    val setOfPossibleAdds: Set<Int> = createSetOfDifferenceBetweenKandIt(arr, sum)
+    return arr.filter { setOfPossibleAdds.contains(it) }.map { Pair(it, sum - it) }
+}
+
+private fun createSetOfDifferenceBetweenKandIt(arr: Array<Int>, sum: Int): Set<Int> {
+    return arr.map { sum - it }.toHashSet()
+}
+
+fun main(args: Array<String>) {
+    val sumTarget = 8
+    val arr = arrayOf(1,2,4,9,7,6,8)
+    println("Should print [(1, 7), (2, 6), (4, 4), (7, 1), (6, 2)]: Actual: ${findPairsWithSum(arr, sumTarget)}")
+}
