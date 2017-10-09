2cbe9394091e33eaac3195acd24ea34a20f1f597
Jason Roell <jason.roell@kroger.com>
2017-10-09 18:48:59 -0400

    Clean up

--- src/ModerateDifficultQuestions/PondSizes.kt
+++ src/ModerateDifficultQuestions/PondSizes.kt
@@ -8,3 +8,19 @@ package ModerateDifficultQuestions
 // above sea level. A value of zero indicates water. A pond is a region of water connected vertically, horizontally,
 // or diagonally. The size of the pond is the total number of connected water cells. Write a method to compute the
 // sizes of all ponds in the matrix
+
+fun computeSizesOfAllPonds(pond: Array<Array<Int>>): List<Pair<String, Int>> {
+
+    return emptyList()
+}
+
+fun main(args: Array<String>) {
+    val pond = arrayOf(arrayOf( 1,0,0,1,1,1,
+                                1,1,1,1,0,0,
+                                1,0,0,1,0,0,
+                                1,0,0,0,1,1,
+                                1,1,1,0,1,0,
+                                1,1,0,1,1,0)
+
+    println("Should print...: Actual: ${computeSizesOfAllPonds(pond)}")
+}
