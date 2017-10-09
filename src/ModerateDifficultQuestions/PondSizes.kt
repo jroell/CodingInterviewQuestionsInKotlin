40a93bc12bd024f6624fd857c92cc300a299e3b3
Jason Roell <jason.roell@kroger.com>
2017-10-09 19:40:56 -0400

    Add some work for PondSizes

--- src/ModerateDifficultQuestions/PondSizes.kt
+++ src/ModerateDifficultQuestions/PondSizes.kt
@@ -8,19 +8,64 @@ package ModerateDifficultQuestions
 // above sea level. A value of zero indicates water. A pond is a region of water connected vertically, horizontally,
 // or diagonally. The size of the pond is the total number of connected water cells. Write a method to compute the
 // sizes of all ponds in the matrix
+class PondFinder {
 
-fun computeSizesOfAllPonds(pond: Array<Array<Int>>): List<Pair<String, Int>> {
+    val ponds = arrayListOf<Pond>()
+    val pondNodes = arrayListOf<PondNode>()
+    var pondCounter = 0
 
-    return emptyList()
+    fun computeSizesOfAllPonds(pond: List<List<Int>>): List<Pond> {
+        // go box by box checking to see if any neibors are a pond (1). If they are then
+        // add them to a queue. Once you check all neighbors then start checking the nodes in the
+        // queue. Repeat the process of adding to the queue with these neigbors.
+        // Mark all nodes as visited and apart of the same pond.
+
+
+        return emptyList()
+    }
+
+
+    fun getNeighborsThatHavePond(pond: List<List<Int>>, location: Pair<Int, Int>, currentPondId: Int = 0): List<Pair<Int, Int>> {
+
+        val pondId = if (currentPondId == 0) pondCounter + 1 else currentPondId
+        val (x, y) = location
+        val potentialPondNode = pond.getOrNull(x - 1)?.getOrNull(y + 1)
+        if (potentialPondNode == 1) {
+            createOrIgnorePondNode(x - 1, y + 1, pondId)
+        }
+
+    }
+
+    private fun createOrIgnorePondNode(x: Int, y: Int, pondId: Int) {
+        if (pondNodes.any { it.location == Pair(x, y) }) return
+
+        pondNodes.add(PondNode(pondId, true, Pair(x, y)))
+        if (ponds.any { it.pondId == pondId }){
+            val pond = ponds.first { it.pondId == pondId }
+            pond.size += 1
+        }
+        else {
+            val pond = Pond(pondId, 1).pondNodes.add(PondNode(x,y))
+
+        }
+    }
+
+    data class PondNode(val pondId: Int = 0, val visited: Boolean = false, val location: Pair<Int, Int>)
+
+    data class Pond(val pondId: Int, var size: Int) {
+        val pondNodes = arrayListOf<PondNode>()
+    }
+
 }
 
 fun main(args: Array<String>) {
-    val pond = arrayOf(arrayOf( 1,0,0,1,1,1,
-                                1,1,1,1,0,0,
-                                1,0,0,1,0,0,
-                                1,0,0,0,1,1,
-                                1,1,1,0,1,0,
-                                1,1,0,1,1,0)
+    val pondFinder = PondFinder()
+    val pond = listOf(listOf(1, 0, 0, 1, 1, 1,
+            1, 1, 1, 1, 0, 0,
+            1, 0, 0, 1, 0, 0,
+            1, 0, 0, 0, 1, 1,
+            1, 1, 1, 0, 1, 0,
+            1, 1, 0, 1, 1, 0))
 
-    println("Should print...: Actual: ${computeSizesOfAllPonds(pond)}")
+    println("Should print...: Actual: ${pondFinder.computeSizesOfAllPonds(pond)}")
 }
